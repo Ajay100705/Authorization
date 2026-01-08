e48e5611f3f9eb123e6687211f2c9feefedafacf
Ajay Chandel <chandelajay2812@gmail.com>
2026-01-08 23:59:47 +0530

    add signals.py and make model-CustomUser

--- user/models.py
+++ user/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from django.utils import timezone
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPE = (
+        ('student' , 'student'),
+        ('teacher' , 'teacher'),
+        ('admin' , 'admin')
+    )
+    
+    id = models.UUIDField(default=uuid4, primary_key=True, null=False, blank=False, unique=True, editable=False)
+    user_type = models.CharField(max_length=20,choices=USER_TYPE, default='student')
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    user_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f" {self.user_id} - {self.get_full_name()} )"
+    
+    def generate_user_id(user_type, instance):
+
+        current_year = timezone.now().strftime('%y')
+
+        prefix = {
+            'student' : 'STU',
+                                                                                                                                                        
+        }
+
+    
+
+
+
+
+
---
+++ user/signals.py
@@ -0,0 +1,39 @@
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.utils import timezone
+from .models import CustomUser
+
+def generate_user_id(user_type):
+
+    curent_year = timezone.now().strftime('%Y')
+
+    prefix_map ={
+        'student' : 'STU',
+        'teacher' : 'TRE',
+        'admin' : 'ADM',
+    }
+
+    prefix = prefix_map.get('user_type', 'USE')
+
+    last_user = (CustomUser.objects.filter(user_type = user_type, user_id__startwith = f"{prefix}{curent_year}")
+                 .order_by('-user_id').first())
+    
+    if last_user :
+        last_sequence = int(last_user.user_id[-4:])
+        new_sequence = last_sequence + 1
+    else:
+        new_sequence = 1
+
+    return f"{prefix}{curent_year}{new_sequence:04d}"
+
+
+@receiver(pre_save, sender=CustomUser)
+def User_id_Signal(sennder, instance, **kwargs):
+    if not instance.user_id and instance.user_type:
+        instance.user_id = generate_user_id(instance.user_type)
+
+    if not instance.username:
+        instance.username = instance.user_id
+
+    if instance._state.adding and not instance.password:
+        instance.set_password("welcome@123")
